8c320567a8ae500bad63cc7d4208ce0fb8140885
Noah <noahoharraaqj155@gmail.com>
2026-04-02 06:55:55 +0300

    fix model name

--- netlify/functions/decompose.js
+++ netlify/functions/decompose.js
@@ -78,7 +78,7 @@ ${userContext ? 'Контекст: ' + userContext : ''}
         method: 'POST',
         headers: { 'Authorization': 'Bearer ' + apiKey, 'Content-Type': 'application/json' },
         body: JSON.stringify({
-          model: 'meta-llama/llama-3.3-8b-instruct:free',
+          model: 'meta-llama/llama-3.1-8b-instruct:free',
           messages: [
             {
               role: 'user',
